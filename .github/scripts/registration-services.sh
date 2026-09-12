fb50ec609ce33b8cc58aa27d9f29334cca712846
YJBeetle <yjbeetle@gmail.com>
2026-09-12 16:08:17 +0800

    Query the runner registry view

--- .github/scripts/registration-services.sh
+++ .github/scripts/registration-services.sh
@@ -24,22 +24,21 @@ runner_windows=$(winepath -w "$test_root/RegistrationRunner.exe")
 probe_windows=$(winepath -w "$test_root/RegistrationProbe.dll")
 WINE_MONO_AOT=none timeout -k 5 120 wine "$runner_windows" register "$probe_windows" > "$test_root/register.log" 2>&1
 
-wine reg query 'HKCR\MonoTests.RegistrationServices.Probe' > "$test_root/progid.log"
-wine reg query 'HKCR\CLSID\{5E4466A3-2BA4-414E-B70B-317D91BE57CC}\InprocServer32' > "$test_root/clsid.log"
-wine reg query 'HKCR\CLSID\{5E4466A3-2BA4-414E-B70B-317D91BE57CC}\MonoRegistrationProbe' > "$test_root/callback.log"
+wine reg query 'HKCR\MonoTests.RegistrationServices.Probe' /reg:32 > "$test_root/progid.log"
+wine reg query 'HKCR\CLSID\{5E4466A3-2BA4-414E-B70B-317D91BE57CC}\InprocServer32' /reg:32 > "$test_root/clsid.log"
+wine reg query 'HKCR\CLSID\{5E4466A3-2BA4-414E-B70B-317D91BE57CC}\MonoRegistrationProbe' /reg:32 > "$test_root/callback.log"
 grep -q 'mscoree.dll' "$test_root/clsid.log"
 grep -q 'MonoTests.RegistrationServices.ProbeObject' "$test_root/clsid.log"
 grep -q 'registered' "$test_root/callback.log"
 
 WINE_MONO_AOT=none timeout -k 5 120 wine "$runner_windows" unregister "$probe_windows" > "$test_root/unregister.log" 2>&1
-if wine reg query 'HKCR\MonoTests.RegistrationServices.Probe' > "$test_root/progid-after-unregister.log" 2>&1; then
+if wine reg query 'HKCR\MonoTests.RegistrationServices.Probe' /reg:32 > "$test_root/progid-after-unregister.log" 2>&1; then
     echo 'ProgID remains after unregistration' >&2
     exit 1
 fi
-if wine reg query 'HKCR\CLSID\{5E4466A3-2BA4-414E-B70B-317D91BE57CC}' > "$test_root/clsid-after-unregister.log" 2>&1; then
+if wine reg query 'HKCR\CLSID\{5E4466A3-2BA4-414E-B70B-317D91BE57CC}' /reg:32 > "$test_root/clsid-after-unregister.log" 2>&1; then
     echo 'CLSID remains after unregistration' >&2
     exit 1
 fi
 
 echo 'RegistrationServices integration test passed'
-
